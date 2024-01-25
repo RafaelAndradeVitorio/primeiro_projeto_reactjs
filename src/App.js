d43a817755905cdb8395bf58277bafb905cfb5ee
RafaelAndradeVitorio <raafael4212@gmail.com>
2024-01-25 12:01:30 -0300

    Componetização do Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Rick Sanchez</h2>
-      <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
-    </div>
+<>
+<Card></Card>
+</>
   );
 }
 
